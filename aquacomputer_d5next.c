38cb1238e30460548d8560a72b59ee129f65601f
Aleksa Savić <10512545+aleksamagicka@users.noreply.github.com>
2022-01-27 18:56:24 +0100

    Pump and fan PWM support, tested

--- aquacomputer_d5next.c
+++ aquacomputer_d5next.c
@@ -3,63 +3,82 @@
  * hwmon driver for Aquacomputer D5 Next watercooling pump
  *
  * The D5 Next sends HID reports (with ID 0x01) every second to report sensor values
- * (coolant temperature, pump and fan speed, voltage, current and power). It responds to
- * Get_Report requests, but returns a dummy value of no use.
+ * (coolant temperature, pump and fan speed, voltage, current and power). This driver
+ * also allows controlling the pump and fan speed via PWM.
  *
  * Copyright 2021 Aleksa Savic <savicaleksa83@gmail.com>
  */
 
 #include <asm/unaligned.h>
+#include <linux/crc16.h>
 #include <linux/debugfs.h>
 #include <linux/hid.h>
 #include <linux/hwmon.h>
 #include <linux/jiffies.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/seq_file.h>
 
-#define DRIVER_NAME			"aquacomputer-d5next"
+#define DRIVER_NAME			"aquacomputer_d5next"
 
-#define D5NEXT_STATUS_REPORT_ID	0x01
-#define D5NEXT_STATUS_UPDATE_INTERVAL	(2 * HZ) /* In seconds */
+#define SENSOR_REPORT_ID		0x01
+#define STATUS_REPORT_ID		0x03
+#define SECONDARY_STATUS_REPORT_ID	0x02
+#define STATUS_UPDATE_INTERVAL		(2 * HZ) /* In seconds */
 
-/* Register offsets for the D5 Next pump */
+#define STATUS_REPORT_SIZE		809
+#define SECONDARY_STATUS_REPORT_SIZE	11
 
-#define D5NEXT_SERIAL_FIRST_PART	3
-#define D5NEXT_SERIAL_SECOND_PART	5
-#define D5NEXT_FIRMWARE_VERSION	13
-#define D5NEXT_POWER_CYCLES		24
+/* Start index and length of the part of the report that gets checksummed */
 
-#define D5NEXT_COOLANT_TEMP		87
+#define STATUS_REPORT_CHECKSUM_START	0x01
+#define STATUS_REPORT_CHECKSUM_LENGTH	0x326
 
-#define D5NEXT_PUMP_SPEED		116
-#define D5NEXT_FAN_SPEED		103
+/* Offset of the checksum in the status report */
 
-#define D5NEXT_PUMP_POWER		114
-#define D5NEXT_FAN_POWER		101
+#define STATUS_REPORT_CHECKSUM		807
 
-#define D5NEXT_PUMP_VOLTAGE		110
-#define D5NEXT_FAN_VOLTAGE		97
-#define D5NEXT_5V_VOLTAGE		57
+/* Register offsets for the D5 Next pump sensor report */
 
-#define D5NEXT_PUMP_CURRENT		112
-#define D5NEXT_FAN_CURRENT		99
+#define SERIAL_FIRST_PART	3
+#define SERIAL_SECOND_PART	5
+#define FIRMWARE_VERSION	13
+#define POWER_CYCLES		24
+
+#define COOLANT_TEMP		87
+
+#define PUMP_SPEED		116
+#define FAN_SPEED		103
+
+#define PUMP_POWER		114
+#define FAN_POWER		101
+
+#define PUMP_VOLTAGE		110
+#define FAN_VOLTAGE		97
+#define PLUS_5V_VOLTAGE		57
+
+#define PUMP_CURRENT		112
+#define FAN_CURRENT		99
+
+#define OUTPUT_PUMP_SPEED	0x97
+#define OUTPUT_FAN_SPEED	0x42
 
 /* Labels for provided values */
 
-#define L_COOLANT_TEMP			"Coolant temp"
+#define L_COOLANT_TEMP		"Coolant temp"
 
-#define L_PUMP_SPEED			"Pump speed"
-#define L_FAN_SPEED			"Fan speed"
+#define L_PUMP_SPEED		"Pump speed"
+#define L_FAN_SPEED		"Fan speed"
 
-#define L_PUMP_POWER			"Pump power"
-#define L_FAN_POWER			"Fan power"
+#define L_PUMP_POWER		"Pump power"
+#define L_FAN_POWER		"Fan power"
 
-#define L_PUMP_VOLTAGE			"Pump voltage"
-#define L_FAN_VOLTAGE			"Fan voltage"
-#define L_5V_VOLTAGE			"+5V voltage"
+#define L_PUMP_VOLTAGE		"Pump voltage"
+#define L_FAN_VOLTAGE		"Fan voltage"
+#define L_5V_VOLTAGE		"+5V voltage"
 
-#define L_PUMP_CURRENT			"Pump current"
-#define L_FAN_CURRENT			"Fan current"
+#define L_PUMP_CURRENT		"Pump current"
+#define L_FAN_CURRENT		"Fan current"
 
 static const char *const label_speeds[] = {
 	L_PUMP_SPEED,
@@ -82,10 +101,18 @@ static const char *const label_current[] = {
 	L_FAN_CURRENT,
 };
 
+/* Contents of the HID report that the official software always sends after writing values */
+
+static u8 secondary_status_report[] = {
+	0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x34, 0xC6
+};
+
 struct d5next_data {
 	struct hid_device *hdev;
 	struct device *hwmon_dev;
 	struct dentry *debugfs;
+	struct mutex mutex;
+	u8 *buffer;
 	s32 temp_input;
 	u16 speed_input[2];
 	u32 power_input[2];
@@ -100,15 +127,16 @@ struct d5next_data {
 static umode_t d5next_is_visible(const void *data, enum hwmon_sensor_types type, u32 attr,
 				 int channel)
 {
-	return 0444;
+	return 0777; // TODO
 }
 
 static int d5next_read(struct device *dev, enum hwmon_sensor_types type, u32 attr, int channel,
 		       long *val)
 {
+	int ret;
 	struct d5next_data *priv = dev_get_drvdata(dev);
 
-	if (time_after(jiffies, priv->updated + D5NEXT_STATUS_UPDATE_INTERVAL))
+	if (time_after(jiffies, priv->updated + STATUS_UPDATE_INTERVAL))
 		return -ENODATA;
 
 	switch (type) {
@@ -120,6 +148,34 @@ static int d5next_read(struct device *dev, enum hwmon_sensor_types type, u32 att
 		break;
 	case hwmon_power:
 		*val = priv->power_input[channel];
+		break;
+	case hwmon_pwm:
+		/* Request the status report and extract curent PWM values */
+
+		mutex_lock(&priv->mutex);
+
+		memset(priv->buffer, 0x00, STATUS_REPORT_SIZE);
+		ret = hid_hw_raw_request(priv->hdev, STATUS_REPORT_ID, priv->buffer, STATUS_REPORT_SIZE, HID_FEATURE_REPORT, HID_REQ_GET_REPORT);
+		if (ret < 0)
+			goto unlock;
+
+		switch (channel)
+		{
+		case 0:
+			*val = get_unaligned_be16(priv->buffer + OUTPUT_PUMP_SPEED);
+			break;
+		case 1:
+			*val = get_unaligned_be16(priv->buffer + OUTPUT_FAN_SPEED);
+			break;
+		default:
+			break;
+		}
+
+		*val = DIV_ROUND_CLOSEST(*val*255, 100 * 100);
+
+unlock:
+		mutex_unlock(&priv->mutex);
+
 		break;
 	case hwmon_in:
 		*val = priv->voltage_input[channel];
@@ -160,15 +216,101 @@ static int d5next_read_string(struct device *dev, enum hwmon_sensor_types type, 
 	return 0;
 }
 
+static int d5next_set_pwm(struct d5next_data *dev, int channel, long val)
+{
+	int ret;
+	u16 checksum = 0xffff; /* Init value for CRC-16/USB */
+
+	if (val < 0 || val > 255)
+		return -EINVAL;
+
+	mutex_lock(&dev->mutex);
+
+	/* Convert to 0-100 range, then multiply by 100, since that's what the pump expects */
+
+	val = DIV_ROUND_CLOSEST(val * 100 * 100, 255);
+
+	/*
+	 * Request a complete config report from the pump by sending a GET_FEATURE_REPORT and
+	 * store it in dev->buffer
+	*/
+	memset(dev->buffer, 0x00, STATUS_REPORT_SIZE);
+	ret = hid_hw_raw_request(dev->hdev, STATUS_REPORT_ID, dev->buffer, STATUS_REPORT_SIZE, HID_FEATURE_REPORT, HID_REQ_GET_REPORT);
+
+	if (ret < 0)
+		goto unlock_and_return;
+
+	/*
+	 * Set pump or fan PWM values, accordingly. We only modify those values so that any other
+	 * settings, such as RGB lights, stay untouched
+	 */
+
+	// TODO: Ensure that the pump is configured to follow a single value, which is set below, so reset it from any other 'curve' modes!
+
+	switch (channel) {
+	case 0:
+		put_unaligned_be16(val, dev->buffer + OUTPUT_PUMP_SPEED);
+		break;
+	case 1:
+		put_unaligned_be16(val, dev->buffer + OUTPUT_FAN_SPEED);
+		break;
+	default:
+		break;
+	}
+
+	checksum = crc16(checksum, dev->buffer + STATUS_REPORT_CHECKSUM_START, STATUS_REPORT_CHECKSUM_LENGTH);
+ 	checksum ^= 0xffff; /* Xorout value for CRC-16/USB */
+
+	/* Place the new checksum at the end of the report */
+	put_unaligned_be16(checksum, dev->buffer + STATUS_REPORT_CHECKSUM);
+
+	/* Send the patched up report back to the pump */
+	ret = hid_hw_raw_request(dev->hdev, STATUS_REPORT_ID, dev->buffer, STATUS_REPORT_SIZE, HID_FEATURE_REPORT, HID_REQ_SET_REPORT);
+	if (ret < 0)
+		goto unlock_and_return;
+
+	/* The official software sends this report after every change, so do it here as well */
+	ret = hid_hw_raw_request(dev->hdev, SECONDARY_STATUS_REPORT_ID, secondary_status_report, SECONDARY_STATUS_REPORT_SIZE, HID_FEATURE_REPORT, HID_REQ_SET_REPORT);
+	if (ret < 0)
+		goto unlock_and_return;
+
+unlock_and_return:
+	mutex_unlock(&dev->mutex);
+	return ret;
+}
+
+static int d5next_write(struct device *dev, enum hwmon_sensor_types type, u32 attr,
+			      int channel, long val)
+{
+	struct d5next_data *priv = dev_get_drvdata(dev);
+
+	switch (type) {
+	case hwmon_pwm:
+		switch (attr) {
+		case hwmon_pwm_input:
+			return d5next_set_pwm(priv, channel, val);
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return -EOPNOTSUPP;
+}
+
 static const struct hwmon_ops d5next_hwmon_ops = {
 	.is_visible = d5next_is_visible,
 	.read = d5next_read,
 	.read_string = d5next_read_string,
+	.write = d5next_write
 };
 
 static const struct hwmon_channel_info *d5next_info[] = {
 	HWMON_CHANNEL_INFO(temp, HWMON_T_INPUT | HWMON_T_LABEL),
 	HWMON_CHANNEL_INFO(fan, HWMON_F_INPUT | HWMON_F_LABEL, HWMON_F_INPUT | HWMON_F_LABEL),
+	HWMON_CHANNEL_INFO(pwm, HWMON_PWM_INPUT, HWMON_PWM_INPUT),
 	HWMON_CHANNEL_INFO(power, HWMON_P_INPUT | HWMON_P_LABEL, HWMON_P_INPUT | HWMON_P_LABEL),
 	HWMON_CHANNEL_INFO(in, HWMON_I_INPUT | HWMON_I_LABEL, HWMON_I_INPUT | HWMON_I_LABEL,
 			   HWMON_I_INPUT | HWMON_I_LABEL),
@@ -181,39 +323,41 @@ static const struct hwmon_chip_info d5next_chip_info = {
 	.info = d5next_info,
 };
 
+/* Parses sensor reports which the pump automatically sends every second */
+
 static int d5next_raw_event(struct hid_device *hdev, struct hid_report *report, u8 *data, int size)
 {
 	struct d5next_data *priv;
 
-	if (report->id != D5NEXT_STATUS_REPORT_ID)
+	if (report->id != SENSOR_REPORT_ID)
 		return 0;
 
 	priv = hid_get_drvdata(hdev);
 
-	/* Info provided with every report */
+	/* Info provided with every sensor report */
 
-	priv->serial_number[0] = get_unaligned_be16(data + D5NEXT_SERIAL_FIRST_PART);
-	priv->serial_number[1] = get_unaligned_be16(data + D5NEXT_SERIAL_SECOND_PART);
+	priv->serial_number[0] = get_unaligned_be16(data + SERIAL_FIRST_PART);
+	priv->serial_number[1] = get_unaligned_be16(data + SERIAL_SECOND_PART);
 
-	priv->firmware_version = get_unaligned_be16(data + D5NEXT_FIRMWARE_VERSION);
-	priv->power_cycles = get_unaligned_be32(data + D5NEXT_POWER_CYCLES);
+	priv->firmware_version = get_unaligned_be16(data + FIRMWARE_VERSION);
+	priv->power_cycles = get_unaligned_be32(data + POWER_CYCLES);
 
 	/* Sensor readings */
 
-	priv->temp_input = get_unaligned_be16(data + D5NEXT_COOLANT_TEMP) * 10;
+	priv->temp_input = get_unaligned_be16(data + COOLANT_TEMP) * 10;
 
-	priv->speed_input[0] = get_unaligned_be16(data + D5NEXT_PUMP_SPEED);
-	priv->speed_input[1] = get_unaligned_be16(data + D5NEXT_FAN_SPEED);
+	priv->speed_input[0] = get_unaligned_be16(data + PUMP_SPEED);
+	priv->speed_input[1] = get_unaligned_be16(data + FAN_SPEED);
 
-	priv->power_input[0] = get_unaligned_be16(data + D5NEXT_PUMP_POWER) * 10000;
-	priv->power_input[1] = get_unaligned_be16(data + D5NEXT_FAN_POWER) * 10000;
+	priv->power_input[0] = get_unaligned_be16(data + PUMP_POWER) * 10000;
+	priv->power_input[1] = get_unaligned_be16(data + FAN_POWER) * 10000;
 
-	priv->voltage_input[0] = get_unaligned_be16(data + D5NEXT_PUMP_VOLTAGE) * 10;
-	priv->voltage_input[1] = get_unaligned_be16(data + D5NEXT_FAN_VOLTAGE) * 10;
-	priv->voltage_input[2] = get_unaligned_be16(data + D5NEXT_5V_VOLTAGE) * 10;
+	priv->voltage_input[0] = get_unaligned_be16(data + PUMP_VOLTAGE) * 10;
+	priv->voltage_input[1] = get_unaligned_be16(data + FAN_VOLTAGE) * 10;
+	priv->voltage_input[2] = get_unaligned_be16(data + PLUS_5V_VOLTAGE) * 10;
 
-	priv->current_input[0] = get_unaligned_be16(data + D5NEXT_PUMP_CURRENT);
-	priv->current_input[1] = get_unaligned_be16(data + D5NEXT_FAN_CURRENT);
+	priv->current_input[0] = get_unaligned_be16(data + PUMP_CURRENT);
+	priv->current_input[1] = get_unaligned_be16(data + FAN_CURRENT);
 
 	priv->updated = jiffies;
 
@@ -252,6 +396,23 @@ static int power_cycles_show(struct seq_file *seqf, void *unused)
 }
 DEFINE_SHOW_ATTRIBUTE(power_cycles);
 
+static int raw_buffer_show(struct seq_file *seqf, void *unused)
+{
+	struct d5next_data *priv = seqf->private;
+
+	int i;
+
+	for (i = 0; i < STATUS_REPORT_SIZE; i++)
+	{
+		seq_printf(seqf, "%02x ", priv->buffer[i]);
+	}
+
+	seq_printf(seqf, "\n");
+
+	return 0;
+}
+DEFINE_SHOW_ATTRIBUTE(raw_buffer);
+
 static void d5next_debugfs_init(struct d5next_data *priv)
 {
 	char name[32];
@@ -262,6 +423,7 @@ static void d5next_debugfs_init(struct d5next_data *priv)
 	debugfs_create_file("serial_number", 0444, priv->debugfs, priv, &serial_number_fops);
 	debugfs_create_file("firmware_version", 0444, priv->debugfs, priv, &firmware_version_fops);
 	debugfs_create_file("power_cycles", 0444, priv->debugfs, priv, &power_cycles_fops);
+	debugfs_create_file("raw_buffer", 0444, priv->debugfs, priv, &raw_buffer_fops);
 }
 
 #else
@@ -281,10 +443,11 @@ static int d5next_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	if (!priv)
 		return -ENOMEM;
 
-	priv->hdev = hdev;
-	hid_set_drvdata(hdev, priv);
+	priv->buffer = devm_kzalloc(&hdev->dev, STATUS_REPORT_SIZE, GFP_KERNEL);
+	if (!priv->buffer)
+		return -ENOMEM;
 
-	priv->updated = jiffies - D5NEXT_STATUS_UPDATE_INTERVAL;
+	priv->updated = jiffies - STATUS_UPDATE_INTERVAL;
 
 	ret = hid_parse(hdev);
 	if (ret)
@@ -297,6 +460,12 @@ static int d5next_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	ret = hid_hw_open(hdev);
 	if (ret)
 		goto fail_and_stop;
+
+	priv->hdev = hdev;
+	hid_set_drvdata(hdev, priv);
+	mutex_init(&priv->mutex);
+
+	hid_device_io_start(hdev);
 
 	priv->hwmon_dev = hwmon_device_register_with_info(&hdev->dev, "d5next", priv,
 							  &d5next_chip_info, NULL);
@@ -320,6 +489,8 @@ fail_and_stop:
 static void d5next_remove(struct hid_device *hdev)
 {
 	struct d5next_data *priv = hid_get_drvdata(hdev);
+
+	mutex_unlock(&priv->mutex);
 
 	debugfs_remove_recursive(priv->debugfs);
 	hwmon_device_unregister(priv->hwmon_dev);
